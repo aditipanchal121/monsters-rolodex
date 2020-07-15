c4f070f08e852861183d4b83994cc436f88ea1e6
Aditi Panchal <aditipanchal121@gmail.com>
2020-07-15 00:42:02 -0500

    Updates

---
+++ precache-manifest.d4e3cdca347fdbc196c59f981dfe5a8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838a33d50a2214e0142807e983c4d903",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f99488c63c4ee8c7d274",
+    "url": "/monsters-rolodex/static/css/main.4c1f0ecc.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f99488c63c4ee8c7d274",
+    "url": "/monsters-rolodex/static/js/main.5cce9b05.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
